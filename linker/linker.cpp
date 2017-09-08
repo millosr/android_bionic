c01f3e06f7e7db21b1d7799fa487ebe6276805c8
mickybart <mickybart@pygoscelis.org>
2017-09-08 12:54:07 -0400

    fuji: support

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -3278,11 +3278,13 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 
 #if !defined(__LP64__)
   if (has_text_relocations) {
+#if !defined(LEGACY_BLOB_COMPATIBLE)
     // Fail if app is targeting M or above.
     if (get_application_target_sdk_version() >= __ANDROID_API_M__) {
       DL_ERR_AND_LOG("\"%s\" has text relocations", get_realpath());
       return false;
     }
+#endif
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
     DL_WARN("\"%s\" has text relocations. This is wasting memory and prevents "
